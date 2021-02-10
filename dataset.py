ae8c8cea1db3b432b036f080dd61c36482294e3f
SeungHyun <67039023+gusanans218@users.noreply.github.com>
2021-02-10 22:22:40 +0900

    GNN

---
+++ dataset.py
@@ -0,0 +1,107 @@
+import os
+import numpy as np
+
+import torch
+import torch.nn as nn
+
+from skimage.transform import resize
+
+import matplotlib.pyplot as plt
+
+from util import *
+
+## Data Loader
+class Dataset(torch.utils.data.Dataset):
+    def __init__(self, data_dir, transform=None, task=None, opts=None):
+        self.data_dir = data_dir
+        self.transform = transform
+        self.task = task
+        self.opts = opts
+        self.to_tensor = ToTensor()
+
+        lst_data = os.listdir(self.data_dir)
+        lst_data = [f for f in lst_data if f.endswith('jpg') | f.endswith('jpeg') | f.endswith('png')]
+        
+        lst_data.sort()
+        self.lst_data = lst_data
+
+    def __len__(self):
+        return len(self.lst_data)
+
+
+    def __getitem__(self, index):
+        img = plt.imread(os.path.join(self.data_dir, self.lst_data[index]))
+        size = img.shape
+
+        if img.dim == 2:
+            img = img[:, :, np.newaxis]
+
+        # 0 ~ 1
+        if img.dtype == np.uint8:
+            img = img / 255.0
+
+        if self.opts[0] == 'direction':
+            if self.opts[1] == 0:
+                data = {'label': img[:, :size[1]//2, :], 'input': img[:, size[1]//2:, :]}
+            elif self.opts[1] == 1:
+                data = {'label': img[:, size[1]//2:, :], 'input': img[:, :size[1]//2, :]}
+        else:
+            data = {'label': img}
+
+        if self.transform:
+            data = self.transform
+        
+        data = self.to_tensor(data)
+
+        return data
+
+
+## numpy => Tensor
+class ToTensor(object):
+    def __call__(self, data):
+        for key, value in data.items():
+            value = value.transpose((2, 0, 1)).astype(np.float32)
+            data[key] = torch.from_numpy(value)
+
+        return data
+
+class Normalization(object):
+    def __init__(self, mean=0.5, std=0.5):
+        self.mean = mean
+        self.std = std
+
+    def __call__(self, data):
+        for key, value in data.items():
+            data[key] = (value - self.mean) / self.std
+
+        return data
+
+class RandomCrop(object):
+    def __init__(self, shape):
+        self.shape = shape
+
+    def __call__(self, data):
+        h, w = data['label'].shape[:2]
+
+        new_h, new_w = self.shape
+
+        top = np.random.randint(0, h - new_h)
+        left = np.random.randint(0, w - new_w)
+
+        id_y = np.arange(top, top + new_h, 1)[:, np.newaxis]
+        id_x = np.arange(left, left + new_w, 1)
+        
+        for key, value in data.items():
+            data[key] = value[id_y, id_x]
+
+        return data
+    
+class Resize(object):
+    def __init__(self, shape):
+        self.shape = shape
+
+    def __call__(self, data):
+        for key, value in data.items():
+            data[key] = resize(value, output_shape=(self.shape[0], self.shape[1], self.shape[2]))
+        
+        return data
